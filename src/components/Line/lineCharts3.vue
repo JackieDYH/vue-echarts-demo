743b971acae8cfa4c7d045ac5f25b4102528d574
Jackie <1824561218@qq.com>
2023-07-06 14:09:19 +0800

    line曲线颜色调整

--- src/components/Line/lineCharts3.vue
+++ src/components/Line/lineCharts3.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: Jackie
  * @Date: 2023-07-06 12:44:25
- * @LastEditTime: 2023-07-06 12:46:35
+ * @LastEditTime: 2023-07-06 14:08:57
  * @LastEditors: Jackie
  * @Description: 曲线图传参
  * @FilePath: /vue-echarts-demo/src/components/Line/lineCharts3.vue
@@ -36,7 +36,6 @@ onMounted(() => {
   console.log(props.data);
   initChart(props.data);
 });
-// 监听
 watch(
   () => props.data,
   (val) => {
@@ -53,7 +52,7 @@ onUnmounted(() => {
 let myChart = '';
 const myEcharts = ref(null);
 
-const initChart = (data) => {
+const initChart = (data, showShadow = true) => {
   console.log(myChart, myEcharts.value, 777);
 
   echarts.init(myEcharts.value).dispose();
@@ -64,55 +63,71 @@ const initChart = (data) => {
   console.log(myChart, myEcharts.value, 888);
 
   // 把配置和数据
-  const myData = {
-    data
-    // data: [
-    //   { value: 335, name: '直接访问' },
-    //   { value: 310, name: '邮件营销' },
-    //   { value: 234, name: '联盟广告' },
-    //   { value: 135, name: '视频广告' },
-    //   { value: 1548, name: '搜索引擎' }
-    // ]
-  };
+  //   const myData = {
+  //     data
+  //     // data: [
+  //     //   { value: 335, name: '直接访问' },
+  //     //   { value: 310, name: '邮件营销' },
+  //     //   { value: 234, name: '联盟广告' },
+  //     //   { value: 135, name: '视频广告' },
+  //     //   { value: 1548, name: '搜索引擎' }
+  //     // ]
+  //   };
+
+  let color = getColor(data);
 
   // 设置option
   const option = {
     xAxis: {
-      type: 'category',
+      type: 'category', //'time'
+      name: '',
+      nameTextStyle: {},
+      boundaryGap: ['0%', '0%'],
+      axisLine: {
+        show: false
+      },
+      axisTick: {
+        show: false //隐藏x轴刻度
+      },
+      splitLine: {
+        show: false //隐藏x轴线
+      },
       boundaryGap: false, //到边界
       // show: false,
-      // axisLine: {
-      //   show: false //隐藏x轴线
-      // },
-      // axisTick: {
-      //   show: false //隐藏x轴刻度
-      // },
-      // 显示坐标
       // axisLabel: {
-      //   show: false
+      //   show: false// 显示坐标
       // },
-      data: myData.data.map((item) => item.name) //['24/3', '25/3', '26/3', '27/3', '28/3', '29/3', '30/3']
+      data: data.map((item) => item.name) //['24/3', '25/3', '26/3', '27/3', '28/3', '29/3', '30/3']
     },
     yAxis: {
+      name: '',
       type: 'value'
       // show: false, //取消显示坐标轴,坐标轴刻度,坐标值(如果是y轴,默认的网格线也会取消显示)
       // splitLine: {
+      //   show: false,
+      //   lineStyle: {
+      //     color: 'rgba(109, 180, 202, 0.3)'
+      //   }
+      // }
+      // axisLine: {
       //   show: false
       // },
-      // 显示坐标
       // axisLabel: {
-      //   show: false
+      //   show: false// 显示坐标
       // }
       // axisTick: {
-      //   show: false //隐藏x轴刻度
+      //   show: false //隐藏y轴刻度
       // }
     },
     tooltip: {
       trigger: 'axis',
+      show: true,
+      // showContent: false,
       axisPointer: {
-        type: 'cross',
-        label: {
-          backgroundColor: '#6a7985'
+        type: 'cross', //'line',
+        // axis: 'x',
+        lineStyle: {
+          color: 'rgba(109, 180, 202, 0.3)'
         }
       }
     },
@@ -125,33 +140,27 @@ const initChart = (data) => {
     },
     series: [
       {
-        data: myData.data.map((item) => item.value), //[820, 932, 901, 934, 1290, 1330, 1320],
+        data: data.map((item) => item.value), //[820, 932, 901, 934, 1290, 1330, 1320],
         type: 'line',
         smooth: true,
+        symbol: 'circle',
+        smooth: true,
+        sampling: 'average',
+        itemStyle: {
+          color: color,
+          borderWidth: 2,
+          borderColor: color,
+          opacity: 0
+        },
+        lineStyle: {
+          color: color,
+          width: 2
+        },
         areaStyle: {
           // 渐变颜色
-          color:
-            // new echarts.graphic.LinearGradient(0, 0, 0, 1, [
-            //   {
-            //     offset: 0,
-            //     color: 'rgba(17,195,132,0.31)'
-            //   },
-            //   {
-            //     offset: 1,
-            //     color: 'rgba(17,195,132,0)'
-            //   }
-            // ]) || 'transparent'
-            new echarts.graphic.LinearGradient(0, 0, 0, 1, [
-              {
-                offset: 0,
-                color: 'rgba(217,61,87,0.31)'
-              },
-              {
-                offset: 1,
-                color: 'rgba(217,61,87,0)'
-              }
-            ])
-        }
+          color: showShadow ? getAreaStyle(data) : 'transparent'
+        },
+        animation: true
       }
     ]
   };
@@ -161,6 +170,72 @@ const initChart = (data) => {
   //   window.addEventListener('resize', () => {
   //     myChart?.resize();
   //   });
+};
+const getMockData = () => {
+  return new Array(20).fill('').map((item, index) => {
+    return [`2023-2-${index + 1}`, Math.random().toFixed(4)];
+    // new Date(`2023,2,${index+1}`)
+    // +new Date(`2023,2,${index+1}`)
+    // 这俩都是不行的。得直接喂字符串进去。乐
+    // 原因应该是如果不是字符串格式，数字会先转Date，然后Date再转字符串的时候，带上了.0，然后safari就识别不了了。
+  });
+};
+/**
+ * 线条颜色
+ * @param {*} list
+ */
+const getColor = (list) => {
+  if (typeof list !== 'object') {
+    list = list.split(',');
+  }
+  let length = list.length;
+  if (length < 2) {
+    return '#11C384';
+  }
+  if (+list[length - 1].value > +list[length - 2].value) {
+    return '#11C384';
+  } else {
+    return '#D93D57';
+  }
+};
+/**
+ * 设置曲线阴影颜色
+ * @param {*} list
+ */
+const getAreaStyle = (list) => {
+  const upColor = new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+    {
+      offset: 0,
+      color: 'rgba(17,195,132,0.31)'
+    },
+    {
+      offset: 1,
+      color: 'rgba(17,195,132,0)'
+    }
+  ]);
+  const downColor = new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+    {
+      offset: 0,
+      color: 'rgba(217,61,87,0.31)'
+    },
+    {
+      offset: 1,
+      color: 'rgba(217,61,87,0)'
+    }
+  ]);
+  if (typeof list !== 'object') {
+    list = list.split(',');
+  }
+  let length = list.length;
+  if (length < 2) {
+    return upColor;
+  }
+  // 按照数组的最后2个值来判断趋势上升/下降
+  if (+list[length - 1].value > +list[length - 2].value) {
+    return upColor;
+  } else {
+    return downColor;
+  }
 };
 </script>
 
